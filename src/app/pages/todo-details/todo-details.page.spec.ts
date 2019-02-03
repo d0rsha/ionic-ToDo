b3c42cb9c304ef1bd2b23a6e526d5b93e8450fff
Anders@Klumpisen <avrethem@gmail.com>
2019-02-03 17:21:07 +0100

    Generate template code

--- src/app/pages/todo-details/todo-details.page.spec.ts
+++ src/app/pages/todo-details/todo-details.page.spec.ts
@@ -1,22 +1,22 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomePage } from './home.page';
+import { TodoDetailsPage } from './todo-details.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('TodoDetailsPage', () => {
+  let component: TodoDetailsPage;
+  let fixture: ComponentFixture<TodoDetailsPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ TodoDetailsPage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
-      .compileComponents();
+    .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(TodoDetailsPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
