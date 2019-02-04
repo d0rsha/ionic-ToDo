4da1851f1f98b84d5038cf77dbf63ea13cb3a781
Anders@Klumpisen <avrethem@gmail.com>
2019-02-04 20:30:59 +0100

    Added form controll

--- src/app/pages/todo-details/todo-details.page.ts
+++ src/app/pages/todo-details/todo-details.page.ts
@@ -2,6 +2,8 @@ import { Todo, TodoService } from './../../services/todo.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NavController, LoadingController } from '@ionic/angular';
+// Validator tools
+import { FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-details',
@@ -11,23 +13,42 @@ import { NavController, LoadingController } from '@ionic/angular';
 export class TodoDetailsPage implements OnInit {
 
   todo: Todo = {
-    task: 'test',
+    task: '',
     createdAt: new Date().getTime(),
-    priority: 2
+    priority: 0
   };
 
   todoId = null;
 
+  signUpForm: FormGroup;
+  userData: { 'task': '', 'createdAt': '', 'priority': '' };
+
+  // Validation error messages that will be displayed for each form field with errors.
+  validation_messages = {
+    'myField': [
+      { type: 'pattern', message: 'Please enter a number like 12345678/00.' }
+    ]
+  };
+
   constructor(private route: ActivatedRoute,
-              private nav: NavController,
-              private todoService: TodoService,
-              private loadingController: LoadingController) { }
+    private nav: NavController,
+    private todoService: TodoService,
+    private loadingController: LoadingController) { }
 
   ngOnInit() {
     this.todoId = this.route.snapshot.params['id'];
-    if (this.todoId)  {
+    if (this.todoId) {
       this.loadTodo();
     }
+
+    const EMAILPATTERN = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+    const DIGITPATTERN = /^[0-9]/i;
+    this.signUpForm = new FormGroup({
+    task: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z ]*'), Validators.minLength(4), Validators.maxLength(10)]),
+    createdAt: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]),
+    priority: new FormControl('', [Validators.required, Validators.pattern(DIGITPATTERN),
+      Validators.minLength(1), Validators.maxLength(2)]),
+    });
   }
 
   async loadTodo() {
@@ -60,6 +81,7 @@ export class TodoDetailsPage implements OnInit {
         this.nav.navigateBack('home');
       });
     }
+    this.todo.priority += 1;
   }
 
 }
